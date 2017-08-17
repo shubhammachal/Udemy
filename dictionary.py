1d27650b68703e4f4d351be4ca91c9b6e9875d50
Shubham Machal <shubham95m@gmail.com>
2017-08-17 13:56:13 +0530

    update dictionary.py

--- dictionary.py
+++ dictionary.py
@@ -1,14 +1,24 @@
 import json,difflib
-from difflib import SequenceMatcher
+from difflib import SequenceMatcher,get_close_matches
 data = json.load(open("data.json"))
 def translate(w):
     w=w.lower()
     if w in data:
         return data[w]
-    elif SequenceMatcher(None,w,word).ratio > 0.8:
-        return data[w]
+    elif len(get_close_matches(w, data.keys())) > 0:
+        yn = input("Did you mean %s instead? Y for yes and N for no: " %get_close_matches(w, data.keys())[0])
+        if yn =="Y":
+            return data[get_close_matches(w,data.keys())[0]]
+        elif yn=="N":
+            return "word doesn't exists. Please double check"
+        else:
+            return "Please enter Y or N."
     else:
         return "The word doesn't exists"
-    return data[w]
 word = input("Enter the word in double quotes: ")
-print(translate(word))
+output=translate(word)
+if type(output) == list:
+    for item in output:
+        print item
+else:
+    print output
